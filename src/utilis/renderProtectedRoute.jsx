6ad43bcd519f1716abbf4acc3bc22d3df474b17d
Lakshay <lakshay12290@gmail.com>
2025-09-06 17:58:15 +0530

    Modify renderProtectedRoute by returning Loader when isLoading is true

--- src/utilis/renderProtectedRoute.jsx
+++ src/utilis/renderProtectedRoute.jsx
@@ -1,14 +1,19 @@
 import toast from "react-hot-toast";
 import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import LoadingBar from "../components/LoadingBar";
 
 
 const RenderProtectedRoute = ({condition,renderPage,fallback,errorMessage, devMode=false}) => {
   const checkFailed = () => {
     toast.error(errorMessage);
     if (!condition) {
-      return <Navigate to={fallback} />;
+      return <Navigate to={fallback} replace />;
     }
   };
+    const {isLoading} = useContext(AuthContext);
+    if(isLoading)   return <LoadingBar/>
 
   return <div>{devMode ? renderPage : condition ? renderPage : checkFailed()}</div>;
 };
