710bfb1a9c471fdc48974f18ad7c955557207b25
Lakshay Mahajan <lakshay12290@gmail.com>
2026-08-24 08:29:27 +0530

    fix: correct conditional logic in getInitials function for email and username validation

--- src/utilis/getInitials.js
+++ src/utilis/getInitials.js
@@ -1,6 +1,5 @@
 export const getInitials = (email, username) => {
-  if (!email || typeof email !== 'string' && !username || typeof username !== 'string') return '??';
-  //username -> lakshay-mahajan || rohan-kumar-singh
+  if ((!email || typeof email !== 'string') && (!username || typeof username !== 'string')) return '??';
   if (username) return username.split('-').map(name => name.charAt(0).toUpperCase()).join('');
   return email.substring(0, 2).toUpperCase();
 };
